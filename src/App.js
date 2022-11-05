cb20f74aed31f07f005ec58491f9d68615337b06
Allen Guan <apguan@gmail.com>
2022-11-05 15:25:57 -0700

    Styles (#1)
    * adding styletron and baseweb
    
    * adding env example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import "./App.css";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
 
 import { calculateDiff, RED, GREEN, GRAY } from "./utilities/comparison";
 import {
@@ -12,29 +14,35 @@ const newText = `hello world wtf bitches
  ok but is this the right approach to the problem?
 `;
 
+const engine = new Styletron();
+
 function App() {
   const diff = calculateDiff(oldText, newText);
 
   return (
-    <div className="App">
-      <p>original</p>
-      <p>{oldText}</p>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <div className="App">
+          <p>original</p>
+          <p>{oldText}</p>
 
-      <p>Diff</p>
-      {diff.colorized.map((parts) => {
-        if (parts.color === GREEN) {
-          return <AddedText text={parts.value} />;
-        }
-        if (parts.color === RED) {
-          return <RemovedText text={parts.value} />;
-        }
-        if (parts.color === GRAY) {
-          return <UnchangedText text={parts.value} />;
-        }
+          <p>Diff</p>
+          {diff.colorized.map((parts) => {
+            if (parts.color === GREEN) {
+              return <AddedText text={parts.value} />;
+            }
+            if (parts.color === RED) {
+              return <RemovedText text={parts.value} />;
+            }
+            if (parts.color === GRAY) {
+              return <UnchangedText text={parts.value} />;
+            }
 
-        return null;
-      })}
-    </div>
+            return null;
+          })}
+        </div>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
