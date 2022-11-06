de35489c4d65110da95d963b552936f95c56d743
apguan <apguan@gmail.com>
2022-11-05 22:40:50 -0700

    adding account signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
-import { LightTheme, BaseProvider, styled, DarkTheme } from "baseui";
+import { BaseProvider, styled, DarkTheme } from "baseui";
 
 import Navbar from "./components/Navbar";
 import HighlightedTextArea from "./components/Input";
 import Difference from "./components/Difference";
-
-import { createFile, createIpfsObj } from "./utilities/ipfs";
 
 const engine = new Styletron();
 
@@ -18,21 +16,20 @@ const Column = styled("div", ({ theme }) => ({
 }));
 
 function App() {
+  const [account, setAccount] = useState();
+
   useEffect(() => {
-    const fileObj = createFile("hello world, IPFS SUck ass", "eatMyShorts.js");
-
-    const runDisShit = async () => {
-      const ipfsObj = await createIpfsObj(fileObj);
-      console.log(ipfsObj);
-    };
-
-    runDisShit();
+    const localAccount = localStorage.getItem("ETH_ACCOUNT");
+    if (localAccount) {
+      setAccount(localAccount);
+    }
   }, []);
+
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={DarkTheme}>
         <div className="App">
-          <Navbar />
+          <Navbar account={account} setAccount={setAccount} />
           <Column>
             <HighlightedTextArea />
             <Difference />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,44 @@
-import * as React from "react";
-import { AppNavBar, setItemActive } from "baseui/app-nav-bar";
+import React from "react";
+import { styled } from "baseui";
+import { MonoHeadingMedium, MonoLabelSmall } from "baseui/typography";
+import { Button } from "baseui/button";
 
-const Navbar = () => {
-  const [mainItems, setMainItems] = React.useState([]);
+const NavbarContainer = styled("div", ({ $theme }) => ({
+  color: $theme.colors.backgroundInversePrimary,
+  backgroundColor: $theme.colors.backgroundPrimary,
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+}));
+
+const Navbar = ({ account, setAccount }) => {
+  const handleClick = async () => {
+    if (window.ethereum) {
+      const req = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      localStorage.setItem("ETH_ACCOUNT", req[0]);
+      setAccount(req[0]);
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
 
   return (
-    <AppNavBar
-      title="De-Git"
-      mainItems={mainItems}
-      onMainItemSelect={(item) => {
-        setMainItems((prev) => setItemActive(prev, item));
-      }}
-      username="Allen Guan"
-    />
+    <NavbarContainer>
+      <MonoHeadingMedium style={{ marginLeft: "5%" }}>De-Git</MonoHeadingMedium>
+      {account ? (
+        <MonoLabelSmall style={{ marginRight: "5%" }}>{account}</MonoLabelSmall>
+      ) : (
+        <Button
+          style={{ height: "50%", marginRight: "5%" }}
+          onClick={handleClick}
+        >
+          Connect Wallet
+        </Button>
+      )}
+    </NavbarContainer>
   );
 };
 
