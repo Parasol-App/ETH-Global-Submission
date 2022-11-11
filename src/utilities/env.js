d646024911d553eefbcc10171811fdd26b011463
Saquib Shahzad <saquib@parasol.xyz>
2022-11-10 23:37:24 -0500

    removed hardcoded config (#12)

--- src/utilities/env.js
+++ src/utilities/env.js
@@ -1,1 +1,4 @@
 export const STORAGE_TOKEN = process.env.REACT_APP_STORAGE_TOKEN;
+export const PUSH_PK = process.env.REACT_APP_PUSH_PK;
+export const PUSH_RECIPIENT_ADDR = process.env.REACT_APP_PUSH_RECIPIENT_ADDR;
+export const PUSH_CHANNEL_ADDR = process.env.REACT_APP_PUSH_CHANNEL_ADDR;
